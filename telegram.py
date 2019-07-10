013a90cd6e801129ed6b065ee1c713392a8e9d7b
Artyshko Mykola <artyshko@hotmail.com>
2019-07-10 22:50:37 +0300

    hotfix

--- telegram.py
+++ telegram.py
@@ -18,8 +18,7 @@ logging.basicConfig(level=logging.INFO,
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 
-#manager = Celery('telegram',broker='redis://smd:1mThquQxrJbyVYVlmLLAmwzLd2t5vDWVO@redis-12274.c52.us-east-1-4.ec2.cloud.redislabs.com:12274')
-manager = Celery('telegram',broker='redis://localhost:6379/0')
+manager = Celery('telegram',broker='redis://smd:1mThquQxrJbyVYVlmLLAmwzLd2t5vDWVO@redis-12274.c52.us-east-1-4.ec2.cloud.redislabs.com:12274')
 
 class BotHandler(object):
 
@@ -30,7 +29,7 @@ class BotHandler(object):
     def __getData(self):
         try:
 
-            with open('.beta', 'rb') as f:
+            with open('.telegram', 'rb') as f:
                 data = pickle.load(f)
 
             self.token = data['token']
