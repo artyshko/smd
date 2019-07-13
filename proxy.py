8b397e471a56d427100d6fc08133a8f68b4debd0
Artyshko Mykola <artyshko@hotmail.com>
2019-07-14 01:25:28 +0300

    fix

--- proxy.py
+++ proxy.py
@@ -16,7 +16,7 @@ def getProxy(log=True):
         collector = get_collector('smd-collector')
 
 
-    proxy = collector.get_proxy()
+    proxy = collector.get_proxy({'code': ('us', 'uk')})
 
     result = {
         'proxy': f'{proxy[5]}://{proxy[0]}:{proxy[1]}',
