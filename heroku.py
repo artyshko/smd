9142e00c51b641ad3df0973f28297de41df156cc
Artyshko Mykola <artyshko@hotmail.com>
2019-07-25 01:25:29 +0300

    added heroku

---
+++ heroku.py
@@ -0,0 +1,36 @@
+import heroku3
+import pickle
+
+
+def restart():
+
+    def getData():
+
+        try:
+
+            with open('.heroku', 'rb') as f:
+                data = pickle.load(f)
+
+            return data['token']
+
+        except:
+
+            sys.exit()
+    try:
+
+        heroku_conn = heroku3.from_key(getData())
+        app = heroku_conn.apps()['spotify-music-downloader-bot']
+        dyno = app.dynos()[0]
+
+        print('RESTARTING_DYNO:DONE')
+
+        dyno.restart()
+
+    except:
+
+        print('RESTARTING_DYNO:ERROR')
+
+
+if __name__ == '__main__':
+
+    restart()
